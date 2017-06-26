c19f792e6a8ca6edd5986431aebf0dadf1a4455b
Jean <jeanvitordepaulo@gmail.com>
2017-06-25 22:23:24 -0300

    Adding code

--- Conv.py
+++ Conv.py
@@ -2,5 +2,19 @@ import PyQt5
 import matplotlib
 matplotlib.use('qt5agg')
 import matplotlib.pyplot as plt
-plt.plot([1,2,3])
+from scipy import misc
+import multiprocessing
+
+
+f = misc.face()
+print (multiprocessing.cpu_count())
+misc.imsave('face.png', f)
+from scipy import ndimage
+face = misc.imread('face.png')
+height, width, channels  = f.shape
+print(width)
+print(height)
+print (channels)
+temp=f[200:400, 100:300,:]
+plt.imshow(temp)
 plt.show()
